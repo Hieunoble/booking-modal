cbc072cf5a36246cecf37a3390192a6e8facaa35
DESKTOP-L5NM2C7\PC <hieunoble@gmail.com>
2023-06-29 10:25:59 +0700

    29 June 23

--- main.js
+++ main.js
@@ -61,6 +61,8 @@ const renderCalendar = () => {
 }
 renderCalendar()
 
+let daysTags = document.querySelectorAll('.days li')
+
 prevNextIcon.forEach(icon => {
   icon.addEventListener("click", () => {
     icon.id === "prev" ? currMonth -= 1 : currMonth += 1
@@ -80,6 +82,19 @@ prevNextIcon.forEach(icon => {
 // Calendar handler
 
 const calendarSelector = document.querySelector('input.date')
+
+const handleSelectDays = (e) => {
+  const selectedDay = e.target.textContent
+  if (selectedDay) {
+    calendarSelector.value = `${selectedDay} ${currentDate.innerText}`
+  }
+}
+
+
+daysTags.forEach((li, e) => {
+  // console.log(li.innerHTML);
+  li.addEventListener('click', (e, li) => handleSelectDays(e, li))
+})
 
 
 
